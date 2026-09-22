6a1c7b77d1da4b3538fd702624aa9705c35bebf1
Leahnora Kedukwakwa <leahnora137@gmail.com>
2026-09-22 21:17:02 +1000

    Add POST items endpoint

--- index.js
+++ index.js
@@ -15,5 +15,13 @@ app.get('/items', (req, res) => res.json(items));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.post('/items', (req, res) => {
+    const name = req.body?.name || 'unnamed';
+    const item = { id: items.length + 1, name };
+
+    items.push(item);
+    res.status(201).json(item);
+});
+
 const port = process.env.PORT || 10000;
 app.listen(port, () => console.log(`Server listening on ${port}`)); 
